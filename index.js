9452bbeded9c15284b81d4061ce87ce52492a01d
Louis Y <louisy603@hotmail.com>
2022-04-08 13:27:48 -0400

    commit tree a3f9ead635a1caccdabaf7ac4a5ed08bec16714d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// const element = document.createElement('div');
+// document.body.append(element);
+
+//const ul = document.createElement('ul');
+
+const mainTag = document.getElementById('main')
+console.log(mainTag)
+mainTag.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'Louis is the champion'
+
+document.body.append(newHeader)
+
+console.log(newHeader)
